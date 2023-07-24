58e521b114b0b8dd88a39c35872d708fd5b81184
zziun2 <zzi_u_n2@naver.com>
2023-07-24 22:17:05 +0900

    디자인 패턴 - observer 패턴

---
+++ src/main/java/com/company/design/ObserverMain.java
@@ -0,0 +1,21 @@
+package com.company.design;
+
+import com.company.design.observer.Button;
+import com.company.design.observer.IButtonListener;
+
+public class ObserverMain {
+    public static void main(String[] args) {
+        Button button = new Button("버튼");
+
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+        button.click("메세지 전달 : click 1");
+        button.click("메세지 전달 : click 2");
+        button.click("메세지 전달 : click 3");
+        button.click("메세지 전달 : click 4");
+    }
+}
